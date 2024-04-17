10351fcd7876cc67b353c132815434b879d3ecae
Anush <anush.shukla@vwo.com>
2024-04-17 12:42:23 +0530

    initial commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div style="text-align: center;">
+    <h1 style="font-size: 24px; margin-bottom: 16px; color: #333;">Welcome to VWO integration with Nuxt.</h1>
+    <p style="font-size: 16px; color: #666;">This publicly accessible <a href="" target="_blank">GitHub code</a> can be referred to for setting up the VWO SmartCode on the website.</p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Page Index
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        {
+          innerHTML: `
+            // Your internal script code here
+            console.log('Internal script executed');
+          `,
+        },
+      ],
+    };
+  },
+};
+</script>
